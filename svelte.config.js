11ee554f3c617b6e5ce585c37932ae2adbdb0f1e
linuxunil <linuxunil@gmail.com>
2025-04-23 12:28:45 -0500

    Fixed adapter options

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+			pages: 'build',
+			assets: 'build'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-		pages: 'build',
-		assets: 'build'
+		}
 	}
 };
 
